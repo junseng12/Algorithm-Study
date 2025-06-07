aee58f8268fedb22bc16cddc341a178e9d5cd86b
junseng12 <junseng12@gmail.com>
2025-06-07 12:08:08 +0900

    5~8Week's Plan Update

---
+++ 2025-06-07/9663_N-Queen/Solution.py
@@ -0,0 +1,19 @@
+# Problem: 9663_N-Queen
+# Date: 2025-06-07
+# Language: Python 3
+
+# 조건
+
+# 가정
+
+# 아이디어 : 
+
+
+import sys
+input = sys.stdin.readline
+
+sys.setrecursionlimit(10**6)
+
+
+N = int(input())
+
